07bf20929c33aba3a936814342f23d14d0a03885
sunbaixin <283122529@qq.com>
2020-09-22 16:42:09 +0800

    fix bug

--- library-ui/src/main/java/com/pep/core/uibase/PhotoPageViewFragment.java
+++ library-ui/src/main/java/com/pep/core/uibase/PhotoPageViewFragment.java
@@ -50,23 +50,21 @@ public class PhotoPageViewFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inPhotoPageViewFragmentflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View            root  = inPhotoPageViewFragmentflater.inflate(R.layout.view_image, container, false);
         final PhotoView image = root.findViewById(R.id.image);
+        final View progress = root.findViewById(R.id.progress);
         String          url   = (String) getArguments().get("url");
         if (url.contains("http://") || url.contains("https://")) {
             PEPImageManager.getInstance().load(this, image, url);
         } else {
             final File file = new File(url);
             if (file.exists()) {
-
-
+                progress.setVisibility(View.VISIBLE);
                 new Thread() {
                     @Override
                     public void run() {
                         long startTime = System.currentTimeMillis();
                         Log.d("PEP", "" + startTime);
-                        final String tempPath = getActivity().getCacheDir() + "/temp_"+file.getName()+".png";
-                        if(!new File(tempPath).exists()){
-                            DecodeFile.decodeFile(file, new File(tempPath));
-                        }
+                        final String tempPath = getActivity().getCacheDir() + "/temp_"+file.getName();
+                        DecodeFile.decodeFile(file, new File(tempPath));
 
                         //使用使用Glide进行加载图片进行加载图片
 //                final Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
@@ -78,6 +76,7 @@ public class PhotoPageViewFragment extends Fragment {
                                 @Override
                                 public void run() {
                                     Glide.with(getActivity()).load(tempPath).into(image);
+                                    progress.setVisibility(View.GONE);
                                 }
                             });
                         }
@@ -100,105 +99,6 @@ public class PhotoPageViewFragment extends Fragment {
         return root;
     }
 
-
-    /**
-     * 图片按比例大小压缩方法
-     *
-     * @param image （根据Bitmap图片压缩）
-     * @return
-     */
-    public static Bitmap compressScale(Bitmap image) {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        image.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-        // 判断如果图片大于1M,进行压缩避免在生成图片（BitmapFactory.decodeStream）时溢出
-        if (baos.toByteArray().length / 1024 > 1024) {
-            baos.reset();// 重置baos即清空baos
-            image.compress(Bitmap.CompressFormat.JPEG, 80, baos);// 这里压缩50%，把压缩后的数据存放到baos中
-        }
-        ByteArrayInputStream  isBm    = new ByteArrayInputStream(baos.toByteArray());
-        BitmapFactory.Options newOpts = new BitmapFactory.Options();
-        // 开始读入图片，此时把options.inJustDecodeBounds 设回true了
-        newOpts.inJustDecodeBounds = true;
-        Bitmap bitmap = BitmapFactory.decodeStream(isBm, null, newOpts);
-        newOpts.inJustDecodeBounds = false;
-        int w = newOpts.outWidth;
-        int h = newOpts.outHeight;
-        // 现在主流手机比较多是800*480分辨率，所以高和宽我们设置为
-        // float hh = 800f;// 这里设置高度为800f
-        // float ww = 480f;// 这里设置宽度为480f
-        float hh = 512f;
-        float ww = 512f;
-        // 缩放比。由于是固定比例缩放，只用高或者宽其中一个数据进行计算即可
-        int be = 1;// be=1表示不缩放
-        if (w > h && w > ww) {// 如果宽度大的话根据宽度固定大小缩放
-            be = (int) (newOpts.outWidth / ww);
-        } else if (w < h && h > hh) { // 如果高度高的话根据高度固定大小缩放
-            be = (int) (newOpts.outHeight / hh);
-        }
-        if (be <= 0)
-            be = 1;
-        newOpts.inSampleSize = be; // 设置缩放比例
-        // newOpts.inPreferredConfig = Config.RGB_565;//降低图片从ARGB888到RGB565
-        // 重新读入图片，注意此时已经把options.inJustDecodeBounds 设回false了
-        isBm = new ByteArrayInputStream(baos.toByteArray());
-        bitmap = BitmapFactory.decodeStream(isBm, null, newOpts);
-        return compressImage(bitmap);// 压缩好比例大小后再进行质量压缩
-        //return bitmap;
-    }
-
-    /**
-     * 质量压缩方法
-     *
-     * @param image
-     * @return
-     */
-    public static Bitmap compressImage(Bitmap image) {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        image.compress(Bitmap.CompressFormat.JPEG, 100, baos);// 质量压缩方法，这里100表示不压缩，把压缩后的数据存放到baos中
-        int options = 90;
-        while (baos.toByteArray().length / 1024 > 100) { // 循环判断如果压缩后图片是否大于100kb,大于继续压缩
-            baos.reset(); // 重置baos即清空baos
-            image.compress(Bitmap.CompressFormat.JPEG, options, baos);// 这里压缩options%，把压缩后的数据存放到baos中
-            options -= 10;// 每次都减少10
-        }
-        ByteArrayInputStream isBm   = new ByteArrayInputStream(baos.toByteArray());// 把压缩后的数据baos存放到ByteArrayInputStream中
-        Bitmap               bitmap = BitmapFactory.decodeStream(isBm, null, null);// 把ByteArrayInputStream数据生成图片
-        return bitmap;
-    }
-
-    /**
-     * 5.采样率压缩（设置图片的采样率，降低图片像素）
-     *
-     * @param filePath
-     * @param file
-     */
-    public static void samplingRateCompress(String filePath, File file) {
-        // 数值越高，图片像素越低
-        int                   inSampleSize = 8;
-        BitmapFactory.Options options      = new BitmapFactory.Options();
-        options.inJustDecodeBounds = false;
-//          options.inJustDecodeBounds = true;//为true的时候不会真正加载图片，而是得到图片的宽高信息。
-        //采样率
-        options.inSampleSize = inSampleSize;
-        Bitmap bitmap = BitmapFactory.decodeFile(filePath, options);
-
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        // 把压缩后的数据存放到baos中
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-        try {
-            if (file.exists()) {
-                file.delete();
-            } else {
-                file.createNewFile();
-            }
-            FileOutputStream fos = new FileOutputStream(file);
-            fos.write(baos.toByteArray());
-            fos.flush();
-            fos.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
 
     /**
